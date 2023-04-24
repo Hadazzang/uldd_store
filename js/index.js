78f111d6a05fac0224fb38fb316df795fde60443
Hadazzang <Hadazzang@gmail.com>
2023-04-24 12:05:17 +0900

    맨 위로 버튼 활성화

--- js/index.js
+++ js/index.js
@@ -5,15 +5,23 @@ $(function () {
 });
 
 // 함수
-$(document).ready(function(){
-  $("#box04 ul li:last").click(function(){
-    $("#box04 .gallery").animate({left: '-1200px'});
-    $("#box04 ul li:last").css({backgroundColor: '#ffd75e'})
-    $("#box04 ul li:first").css({backgroundColor: 'transparent'})
+$(document).ready(function () {
+  $('#box04 ul li:last').click(function () {
+    $('#box04 .gallery').animate({ left: '-1200px' });
+    $('#box04 ul li:last').css({ backgroundColor: '#ffd75e' });
+    $('#box04 ul li:first').css({ backgroundColor: 'transparent' });
   });
-  $("#box04 ul li:first").click(function(){
-    $("#box04 .gallery").animate({left: '0px'});
-    $("#box04 ul li:first").css({backgroundColor: '#ffd75e'})
-    $("#box04 ul li:last").css({backgroundColor: 'transparent'})
+  $('#box04 ul li:first').click(function () {
+    $('#box04 .gallery').animate({ left: '0px' });
+    $('#box04 ul li:first').css({ backgroundColor: '#ffd75e' });
+    $('#box04 ul li:last').css({ backgroundColor: 'transparent' });
   });
 });
+
+$(document).ready(function() {
+  $('footer button').click(function() {
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
+});  
+
